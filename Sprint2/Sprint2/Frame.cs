119ce08fbda48c40adecbd67ee9bb7536a31a6d6
verumyn <98348526+verumyn@users.noreply.github.com>
2022-02-04 18:00:32 -0500

    Leigh's Work on Sprint 2 (Player)

---
+++ Sprint2/Sprint2/Frame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint2
+{
+    class Frame
+    {
+        //rectangle and bitmap
+    }
+}
---
+++ Sprint2/Sprint2/Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint2
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Player player;
+        //singleton sprite factory here
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            //how should we implement this?
+            // TODO: use this.Content to load your game content here
+            //player class should hold all sprites for it?
+            //sprite factory????????
+            Texture2D front_still = Content.Load<Texture2D>("front_still");
+            player = new Player(front_still);
+            //???
+            Texture2D front_move = Content.Load<Texture2D>("front_move");
+            Texture2D side = Content.Load<Texture2D>("side");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            player.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            player.Draw(_spriteBatch);
+            base.Draw(gameTime);
+        }
+    }
+}
