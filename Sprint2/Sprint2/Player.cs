1b6a0070e000f581b12dc49e18062cfc900f9a3f
verumyn <98348526+verumyn@users.noreply.github.com>
2022-02-05 15:31:37 -0500

    Uploaded Proper .cs files to the right directory

---
+++ Sprint2/Sprint2/Player.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint2
+{
+    class Player
+
+        //forwardsSprite
+        //sideSprite
+    {
+        private ISprite sprite = new Sprite();
+        private Vector2 location = new Vector2(400, 200);
+        public Player()
+        {
+            //how in the world does the sprite factory interact with this?
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            sprite.Update();
+        }
+
+        //maybe get location here???
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch, location);
+        }
+
+    }
+}
