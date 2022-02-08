4ce29c6260c6bf0095f1c56efc90a14c67a9c8da
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-02-08 17:49:49 -0500

    cleaned up Leigh's KeyboardC based on Keyi's

--- Sprint0/Sprint0/KeyboardC.cs
+++ Sprint0/Sprint0/KeyboardC.cs
@@ -10,34 +10,6 @@ namespace Sprint0
     {
         private Dictionary<Keys, ICommand> keyboardD = new Dictionary<Keys, ICommand>();
         
-        private QuitCom quitCom = new QuitCom();
-        //Non-moving, non-animated sprite command
-        private NMNACom NMNACom = new NMNACom();
-        //Moving, non-animated sprite command
-        private MNACom MNACom = new MNACom();
-        //Non-moving, animated sprite command
-        private NMACom NMACom = new NMACom();
-        //Moving, animated sprite command
-        private MACom MACom = new MACom();
-
-        private PrevItemCom prevItemCom = new PrevItemCom();
-
-        private NextItemCom nextItemCom = new NextItemCom();
-
-        private AttackCom attackCom = new AttackCom();
-
-        private DamageCom damageCom = new DamageCom();
-
-        private ResetCom resetCom = new ResetCom();
-
-        private PrevBlockCom prevBlockCom = new PrevBlockCom();
-
-        private NextBlockCom nextBlockCom = new NextBlockCom();
-
-        private PrevNPCCom prevNPCCom = new PrevNPCCom();
-
-        private NextNPCCom nextNPCCom = new NextNPCCom();
-
         private KeyboardState oldState;
 
         public KeyboardC()
@@ -62,21 +34,21 @@ namespace Sprint0
 
     public void InitializeController()
         {
-            keyboardD.Add(Keys.Q, quitCom);
-            keyboardD.Add(Keys.D1, NMNACom);
-            keyboardD.Add(Keys.D2, NMACom);
-            keyboardD.Add(Keys.D3, MNACom);
-            keyboardD.Add(Keys.D4, MACom);
-            keyboardD.Add(Keys.U, prevItemCom);
-            keyboardD.Add(Keys.I, nextItemCom);
-            keyboardD.Add(Keys.Z, attackCom);
-            keyboardD.Add(Keys.N, attackCom);
-            keyboardD.Add(Keys.E, damageCom);
-            keyboardD.Add(Keys.R, resetCom);
-            keyboardD.Add(Keys.T, prevBlockCom);
-            keyboardD.Add(Keys.Y, nextBlockCom);
-            keyboardD.Add(Keys.O, prevNPCCom);
-            keyboardD.Add(Keys.P, nextNPCCom);
+            keyboardD.Add(Keys.Q, new QuitCom());
+            keyboardD.Add(Keys.D1, new NMNACom());
+            keyboardD.Add(Keys.D2, new NMACom());
+            keyboardD.Add(Keys.D3, new MNACom());
+            keyboardD.Add(Keys.D4, new MACom());
+            keyboardD.Add(Keys.U, new PrevItemCom());
+            keyboardD.Add(Keys.I, new NextItemCom());
+            keyboardD.Add(Keys.Z, new NextItemCom());
+            keyboardD.Add(Keys.N, new AttackCom());
+            keyboardD.Add(Keys.E, new DamageCom());
+            keyboardD.Add(Keys.R, new ResetCom());
+            keyboardD.Add(Keys.T, new PrevBlockCom());
+            keyboardD.Add(Keys.Y, new NextBlockCom());
+            keyboardD.Add(Keys.O, new PrevNPCCom());
+            keyboardD.Add(Keys.P, new NextNPCCom());
             oldState = Keyboard.GetState();
         }
 
