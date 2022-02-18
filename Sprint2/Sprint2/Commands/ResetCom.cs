6b2b752951d11eb255df8079d6ef9289004a4403
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-02-18 17:18:45 -0500

    reset command is working, and reset methods added

--- Sprint2/Sprint2/Commands/ResetCom.cs
+++ Sprint2/Sprint2/Commands/ResetCom.cs
@@ -2,14 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace Sprint2
 {
 	class ResetCom : ICommand
 	{
 		public void Execute(Player player, Item item, Block block, NPC1 enemy)
 		{
-			System.Windows.Forms.Application.Exit();
+			player.Reset();
+			item.Reset();
+			block.Reset();
+			enemy.SetIndx(0);
+			enemy.SetLocation(new Vector2(50, 50));
 		}
 	}
 }
