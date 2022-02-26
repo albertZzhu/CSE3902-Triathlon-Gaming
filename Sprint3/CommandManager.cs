5a51e3d5b62d065063463898c79fe3821b39107b
Leigh Lynch <leighlynch2020@gmail.com>
2022-02-25 21:09:09 -0500

    Added CommandManager

---
+++ Sprint3/CommandManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Sprint3.Commands
+{
+    class CommandManager
+    {
+        private Dictionary<String, ConstructorInfo> commandConstructors;
+
+        public CommandManager()
+        {
+            commandConstructors = new Dictionary<string, ConstructorInfo>();
+            String[] commandsStrings = File.ReadAllLines("Data/Commands.txt");
+            foreach (String commandName in commandsStrings) {
+                Type type = Type.GetType(commandName);
+                //thinking ahead: i might have to change all the params in the command classes to IGameObject...
+                commandConstructors.Add(commandName, type.GetConstructors()[0]);
+            }
+        }
+    }
+}
