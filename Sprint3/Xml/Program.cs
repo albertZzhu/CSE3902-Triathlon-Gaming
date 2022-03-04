ef2c86a411973c43dd3023557d87565bb1592017
doge555 <60857941+doge555@users.noreply.github.com>
2022-03-04 12:39:42 -0500

    revised xml generator

--- Sprint3/Xml/Program.cs
+++ Sprint3/Xml/Program.cs
@@ -15,51 +15,174 @@ namespace Xml
             String filename = ("E:\\levelData.xml");
 
             XmlTextWriter xmlWriter = new XmlTextWriter(filename, System.Text.Encoding.UTF8);
-
             xmlWriter.Formatting = Formatting.Indented;
-
             XmlDocument xmlDoc = new XmlDocument();
-
+       
             xmlWriter.WriteStartDocument();
-
             xmlWriter.WriteStartElement("Level1");
-
-
+            //create number of rooms 
+            Console.WriteLine("how many number of rooms :");
+            int numR = int.Parse(Console.ReadLine());
             //*loop to generate data for the rooms
-            for (int i = 2; i < 4; i++)
+            for (int i = 1; i <= numR; i++)
             {
-                xmlWriter.WriteStartElement("room");
-
-                xmlWriter.WriteAttributeString("id", XmlConvert.ToString(i));
-
-                //create number of objects 
-                Console.WriteLine("how many number of objects in this room"+ XmlConvert.ToString(i)+" :");
-                int numOb = int.Parse(Console.ReadLine());
-                for (int j = 0; j < numOb; j++) {
-
-
-                    xmlWriter.WriteStartElement("object");
-                    Console.WriteLine("what is the type of this object:");
-                    xmlWriter.WriteElementString("type", Console.ReadLine());
-                    Console.WriteLine("what is the name of this object:");
-                    xmlWriter.WriteElementString("name", Console.ReadLine());
-                    Console.WriteLine("what is the file name of the texture");
-                    xmlWriter.WriteElementString("texture", Console.ReadLine());
-                    xmlWriter.WriteStartElement("position");
-                    Console.WriteLine("What is the X position:");
-                    xmlWriter.WriteElementString("x", Console.ReadLine());
-                    Console.WriteLine("What is the Y position:");
-                    xmlWriter.WriteElementString("y", Console.ReadLine());
+                xmlWriter.WriteStartElement("room" + XmlConvert.ToString(i));
+                xmlWriter.WriteStartElement("type");
+                    //start blocks
+                    Console.WriteLine("how many number of blocks :");
+                    int numB = int.Parse(Console.ReadLine());
+                    xmlWriter.WriteStartElement("blocks");
+                    xmlWriter.WriteAttributeString("num", XmlConvert.ToString(numB));
+                    //generating Bs
+                    for(int j = 0; j < numB; j++)
+                    {   
+                        //start a new B
+                        xmlWriter.WriteStartElement("B");
+                        //asking for loc
+                        xmlWriter.WriteStartElement("loc");
+                        Console.WriteLine("What is the block's X position:");
+                        xmlWriter.WriteElementString("x", Console.ReadLine());
+                        Console.WriteLine("What is the block's Y position:");
+                        xmlWriter.WriteElementString("y", Console.ReadLine());
+                        //end loc
+                        xmlWriter.WriteEndElement();
+                        //asking for texture
+                        Console.WriteLine("What is the Texture of this block:");
+                        xmlWriter.WriteElementString("B_texture", Console.ReadLine());
+                        //end B
+                        xmlWriter.WriteEndElement();
+                    }
+                    //end blocks
                     xmlWriter.WriteEndElement();
-                    xmlWriter.WriteEndElement();
-                }
-                xmlWriter.WriteEndElement();
 
-            }
+                    //start items
+                    Console.WriteLine("how many number of items :");
+                    int numI = int.Parse(Console.ReadLine());
+                    xmlWriter.WriteStartElement("items");
+                    xmlWriter.WriteAttributeString("num", XmlConvert.ToString(numI));
+                    //generating Is
+                    for (int j = 0; j < numB; j++)
+                    {   
+                        //start a new I
+                        xmlWriter.WriteStartElement("I");
+                        //asking for loc
+                        xmlWriter.WriteStartElement("loc");
+                        Console.WriteLine("What is the item's X position:");
+                        xmlWriter.WriteElementString("x", Console.ReadLine());
+                        Console.WriteLine("What is the item's Y position:");
+                        xmlWriter.WriteElementString("y", Console.ReadLine());
+                        //end loc
+                        xmlWriter.WriteEndElement();
+                        //asking for texture
+                        Console.WriteLine("What is the Texture of this item:");
+                        xmlWriter.WriteElementString("I_texture", Console.ReadLine());
+                        //end I
+                        xmlWriter.WriteEndElement();
+                    }
+                    //end items
+                    xmlWriter.WriteEndElement();
+
+                    //start enemy
+                    Console.WriteLine("how many number of enemies :");
+                    int numE = int.Parse(Console.ReadLine());
+                    xmlWriter.WriteStartElement("enemies");
+                    xmlWriter.WriteAttributeString("num", XmlConvert.ToString(numE));
+                    //generating Es
+                    for (int k = 0; k < numE; k++)
+                    {   
+                        //start a new E
+                        xmlWriter.WriteStartElement("E");
+                        Console.WriteLine("do you prefer this enemy movable? (true/false)");
+                        xmlWriter.WriteAttributeString("move", Console.ReadLine());
+                        //asking for the orginal loc
+                        xmlWriter.WriteStartElement("loc");
+                        Console.WriteLine("What is the  enemy's 1's X position:");
+                        xmlWriter.WriteElementString("x", Console.ReadLine());
+                        Console.WriteLine("What is the enemy's 1's Y position:");
+                        xmlWriter.WriteElementString("y", Console.ReadLine());
+                        //end loc
+                        xmlWriter.WriteEndElement();
+                        //asking for the orginal facing direction
+                        Console.WriteLine("What is the enemy's 1's facing direction:");
+                        xmlWriter.WriteElementString("dir", Console.ReadLine());
+                        //asking for different facing textures
+                        xmlWriter.WriteStartElement("E_texture");
+                        Console.WriteLine("What is the enemy's upward texture:");
+                        xmlWriter.WriteElementString("up", Console.ReadLine());
+                        Console.WriteLine("What is the enemy's downward texture:");
+                        xmlWriter.WriteElementString("down", Console.ReadLine());
+                        Console.WriteLine("What is the enemy's rightward texture:");
+                        xmlWriter.WriteElementString("right", Console.ReadLine());
+                        Console.WriteLine("What is the enemy's leftward texture:");
+                        xmlWriter.WriteElementString("left", Console.ReadLine());
+                        //end E_texture
+                        xmlWriter.WriteEndElement();
+                        //asking for different projectile facing textures
+                        xmlWriter.WriteStartElement("projectile");
+                        Console.WriteLine("do you prefer the enemy fireble? (true/false)");
+                        xmlWriter.WriteAttributeString("fire", Console.ReadLine());
+                        Console.WriteLine("What is the projectile upward texture:");
+                        xmlWriter.WriteElementString("up", Console.ReadLine());
+                        Console.WriteLine("What is the projectile downward texture:");
+                        xmlWriter.WriteElementString("down", Console.ReadLine());
+                        Console.WriteLine("What is the projectile rightward texture:");
+                        xmlWriter.WriteElementString("right", Console.ReadLine());
+                        Console.WriteLine("What is the projectile leftward texture:");
+                        xmlWriter.WriteElementString("left", Console.ReadLine());
+                        //end projectile
+                        xmlWriter.WriteEndElement();
+                        //asking for set routes
+                        Console.WriteLine("route set? (true/false)");
+                        if (Convert.ToBoolean(Console.ReadLine()) == true)
+                        {   
+                            //if routes setted, ask for how many locs the enemy can move to let it moving in a loop.
+                            xmlWriter.WriteStartElement("routes");
+                            Console.WriteLine("how many locations:");
+                            int numL = int.Parse(Console.ReadLine());
+                            //generating the individual route
+                            for (int a = 0; a < numL; a++)
+                            {   
+                                //start a new R
+                                xmlWriter.WriteStartElement("R");
+                                //asking for every single step in this route's loc
+                                xmlWriter.WriteStartElement("loc");
+                                Console.WriteLine("What is the enemy's " + a+2 + "'s position:");
+                                xmlWriter.WriteElementString("x", Console.ReadLine());
+                                Console.WriteLine("What is the enemy's " + a+2 + "'s Y position:");
+                                xmlWriter.WriteElementString("y", Console.ReadLine());
+                                //end loc
+                                xmlWriter.WriteEndElement();
+                                //asking for every single facing direction under the current step
+                                Console.WriteLine("What is the enemy's " + a + 2 + "'s facing direction:");
+                                //end R
+                                xmlWriter.WriteEndElement();
+                            }
+                            //end routes
+                            xmlWriter.WriteEndElement();
+                        } else
+                        {   
+                            //if no routes setted for this enemy, just create an empty route tag
+                            xmlWriter.WriteElementString("routes", null);
+                        }
+                        //asking for the enemy's fire timspan
+                        Console.WriteLine("What is the enemy's fire timspan:");
+                        xmlWriter.WriteElementString("timer", Console.ReadLine());
+                        //end E
+                        xmlWriter.WriteEndElement();
+                    }
+                    //end enemy
+                    xmlWriter.WriteEndElement();
+                //end type
                 xmlWriter.WriteEndElement();
-                xmlWriter.WriteEndDocument();
-                xmlWriter.Flush();
-                xmlWriter.Close();
+                //end room
+                xmlWriter.WriteEndElement();
+            }   
+            //end level1
+            xmlWriter.WriteEndElement();
+            //end doc
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Flush();
+            xmlWriter.Close();
                 
 
 
