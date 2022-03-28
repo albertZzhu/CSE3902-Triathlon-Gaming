684609cb48f32ed1d05e20c60d564a3a00216b85
Leigh Lynch <leighlynch2020@gmail.com>
2022-03-28 17:49:08 -0400

    Fixed enum values for Facing

--- Sprint4/State Machines/Facing.cs
+++ Sprint4/State Machines/Facing.cs
@@ -6,7 +6,7 @@ namespace Sprint4.State_Machines
 {
     public enum Facing
     {
-        RIGHT, LEFT, UP, DOWN, NORTHEAST, SOUTHEAST, NORTHWEST, SOUTHWEST, NNWEST, NNEAST, SSWEST, SSEAST
-            //1,2,3,4   10,-10, 11,-9, 12,-8, 13,-7
+        RIGHT, LEFT, UP, DOWN, NORTHEAST = 10, SOUTHEAST = -10, NORTHWEST = 11, SOUTHWEST = -9, NNWEST = 12, NNEAST = -8, SSWEST = 13, SSEAST = -7
+        //1,2,3,4   10,-10, 11,-9, 12,-8, 13,-7
     }
 }
