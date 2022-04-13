9a8f246866e3f9bd21a52a272068271cfee266a5
albertZzhu <zhu.2242@osu.edu>
2022-04-12 20:49:01 -0400

    damaged timer modified

---
+++ Sprint5/AstarPathFinder/PathFinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint4
+{
+	class PathFinder
+	{
+		Vector2 start;
+		Vector2 end;
+
+
+		public PathFinder(Vector2 start, Vector2 target, IBlock[] blocks)
+		{
+
+		}
+	}
+}
